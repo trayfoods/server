43b5b37b2a3f221f56be1d70abb5bd84873c5028
Divuzki <divuzki@gmail.com>
2022-08-06 00:20:31 +0100

    Fixed A Bug

--- trayapp/utils.py
+++ trayapp/utils.py
@@ -22,16 +22,17 @@ def image_resize(image, width, height):
         output_size = (width, height)
         # Create a new resized “thumbnail” version of the image with Pillow
         img.thumbnail(output_size)
-        # Find the file name of the image
-        img_filename = Path(image.file.name).name
-        # Spilt the filename on “.” to get the file extension only
-        img_suffix = Path(image.file.name).name.split(".")[-1]
-        # Use the file extension to determine the file type from the image_types dictionary
-        img_format = image_types[img_suffix]
-        # Save the resized image into the buffer, noting the correct file type
-        buffer = BytesIO()
-        img.save(buffer, format=img_format)
-        # Wrap the buffer in File object
-        file_object = File(buffer)
-        # Save the new resized file as usual, which will save to S3 using django-storages
-        image.save(img_filename, file_object)
+    # Find the file name of the image
+    img_filename = Path(image.file.name).name
+    # Spilt the filename on “.” to get the file extension only
+    img_suffix = Path(image.file.name).name.split(".")[-1]
+    # Use the file extension to determine the file type from the image_types dictionary
+    img_format = image_types[img_suffix]
+    # Save the resized image into the buffer, noting the correct file type
+    buffer = BytesIO()
+    img.save(buffer, format=img_format)
+    img.save(buffer, format="webp")
+    # Wrap the buffer in File object
+    file_object = File(buffer)
+    # Save the new resized file as usual, which will save to S3 using django-storages
+    image.save(img_filename, file_object)
