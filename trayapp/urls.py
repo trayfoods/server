6fa2a50ab6f99d19c9b8b0ab43400ee297817684
Divuzki <divuzki@gmail.com>
2022-08-08 13:01:25 +0100

    Updated Required Packages

--- trayapp/urls.py
+++ trayapp/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-# from django.urls import path
+from django.urls import path
 from graphene_file_upload.django import FileUploadGraphQLView
 # from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from .views import index_view
-from django_distill import distill_path as path
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
