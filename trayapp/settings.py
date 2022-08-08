4a935c4518f1ef196f6d223b58d7dc7f39e3942b
Divuzki <divuzki@gmail.com>
2022-08-08 13:05:01 +0100

    Updated Required Packages

--- trayapp/settings.py
+++ trayapp/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",  # Required for GraphiQL
-    "django_distill",
     "users",
     "product",
 
