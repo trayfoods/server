1f4e8885e1d2dd13e0c338e7be89dec0ec64ed71
Divuzki <divuzki@gmail.com>
2022-08-28 15:16:26 +0100

    Railway-delopyment

--- trayapp/settings.py
+++ trayapp/settings.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
+# load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
