e42f8f6f26f1881745e0d30c4721c6e834d670d0
Divuzki <divuzki@gmail.com>
2022-09-01 04:56:47 +0100

    Fixing `ERR_TOO_MANY_REDIRECTS`

--- trayapp/settings.py
+++ trayapp/settings.py
@@ -30,8 +30,6 @@ else:
 ALLOWED_HOSTS = ["192.168.137.1", "localhost", "%s" %
                  os.getenv("SITE_ORIGIN_URL")]
 if DEBUG == False:
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
     SECURE_SSL_REDIRECT = True
     SECURE_HSTS_SECONDS = 518400
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
