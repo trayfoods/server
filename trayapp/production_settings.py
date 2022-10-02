8cf26e09cfe3b10b9026e071a67359960b3286ba
Divuzki <divuzki@gmail.com>
2022-10-02 23:44:34 +0100

    Move Deployment to Fly.io

--- trayapp/production_settings.py
+++ trayapp/production_settings.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
