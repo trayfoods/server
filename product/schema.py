2e156fc5f549720674277b421ad61982e4172d5f
Divuzki <divuzki@gmail.com>
2023-04-09 23:32:43 +0100

    Fix Recommend Errors

--- product/schema.py
+++ product/schema.py
@@ -91,9 +91,12 @@ class Query(graphene.ObjectType):
             count = count + 1
             items = Item.objects.all().distinct()
             items = items[:count if items.count() >= count else items.count()]
-            if info.context.user.is_authenticated and UserActivity.objects.filter(user_id=info.context.user.id).count() > 2:
-                return recommend_items(info.context.user.id, n=count if (items.count() >= count) else items.count())
-            else:
+            try:
+                if info.context.user.is_authenticated and UserActivity.objects.filter(user_id=info.context.user.id).count() > 2:
+                    return recommend_items(info.context.user.id, n=count if (items.count() >= count) else items.count())
+                else:
+                    return items
+            except:
                 return items
         else:
             GraphQLError("There should be a count param in the items query")
