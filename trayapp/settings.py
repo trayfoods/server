eff648ac7c64bb480aa3fd031c23ba5792ef1f18
Divine Ikhuoria <divuzki@gmail.com>
2023-06-04 23:40:09 +0100

    Updated `GRAPHQL_JWT`

--- trayapp/settings.py
+++ trayapp/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -102,9 +103,10 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 GRAPHQL_JWT = {
-    # "JWT_VERIFY_EXPIRATION": True,
-    # optional
-    "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
+    'JWT_ALLOW_ARGUMENT': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LONG_RUNNING_REFRESH_TOKEN': True,
+    'JWT_REFRESH_TOKEN_EXPIRATION_DELTA': timedelta(days=7),
     "JWT_ALLOW_ANY_CLASSES": [
         "graphql_auth.mutations.Register",
         "graphql_auth.mutations.VerifyAccount",
