4b736bc663f2ad3df31ebee39058c344feace470
Divine Ikhuoria <divuzki@gmail.com>
2023-06-27 05:32:32 +0100

    Fixed Token Refresh Bug & Added Websocket feature

--- trayapp/asgi.py
+++ trayapp/asgi.py
@@ -1,14 +1,24 @@
 import os
+from dotenv import load_dotenv
 
-from channels.routing import ProtocolTypeRouter
+load_dotenv()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from trayapp.middlewares import JwtTokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trayapp.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
+import trayapp.routing
+
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AllowedHostsOriginValidator(
+            JwtTokenAuthMiddleware(URLRouter(trayapp.routing.websocket_urlpatterns))
+        )
 })
