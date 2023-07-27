c47d2a5d72986cbbc31a427dd798b295aeb93f24
Divine Ikhuoria <divuzki@gmail.com>
2023-07-27 02:41:25 +0100

    Updated `Order Payment Webhook`

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
+from django.http import JsonResponse
+
 
 def order_payment_webhook(request):
-    body  = request.body
-    print("", body)
+    if request.method == "POST":
+        body = request.body
+        print("", body)
+        return JsonResponse({"status": "success"})
+    else:
+        return JsonResponse({"status": "failed"})
