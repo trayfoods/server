1d5e82361c528e57ba86a4f1a92f92e30b5db14a
Divine Ikhuoria <divuzki@gmail.com>
2023-07-30 18:04:53 +0100

    Finish `share order payment` view

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from core.views import paystack_webhook_handler, order_redirect_share_view
+from core.views import paystack_webhook_handler
 
 urlpatterns = [
     path("paystack-webhook", paystack_webhook_handler, name="paystack-webhook"),
-    path("share-order/<str:order_id>", order_redirect_share_view, name="share-order"),
 ]
