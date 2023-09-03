d739c19f8efac9239e15b53ac76b811c64c95436
Divine Ikhuoria <divuzki@gmail.com>
2023-09-03 12:32:17 +0100

    Updated

--- users/queries/transactions.py
+++ users/queries/transactions.py
@@ -1,11 +1,16 @@
 import graphene
 from graphene_django.filter import DjangoFilterConnectionField
+from trayapp.permissions import IsAuthenticated, permission_checker
 from users.models import Transaction
-from users.types import TransactionNode
+from users.types import TransactionNode, TransactionType
 
 
 class TransactionQueries(graphene.ObjectType):
+    transaction = graphene.Field(TransactionType, transaction_id=graphene.Int())
     transactions = DjangoFilterConnectionField(TransactionNode)
 
+    @permission_checker([IsAuthenticated])
     def resolve_transactions(self, info, **kwargs):
-        return Transaction.objects.all()
+        user = info.context.user
+        profile = user.profile
+        return Transaction.objects.filter(wallet__user=profile)
