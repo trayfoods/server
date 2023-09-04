86758e340e8fd503878584b8f9b2bd8c17ba161f
Divine Ikhuoria <divuzki@gmail.com>
2023-09-04 19:59:01 +0100

    Updated

--- users/filters.py
+++ users/filters.py
@@ -4,15 +4,19 @@ from users.models import Transaction
 
 class TransactionFilter(FilterSet):
     # add custom filters
-    year = NumberFilter(field_name="created_at", lookup_expr="year")
-    month = NumberFilter(field_name="created_at", lookup_expr="month")
-    week = NumberFilter(field_name="created_at", lookup_expr="week")
+    year = NumberFilter(field_name="created_at", lookup_expr="year")  # eg. 2020, 2021
+    month = NumberFilter(
+        field_name="created_at", lookup_expr="month"
+    )  # eg. 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    day = NumberFilter(
+        field_name="created_at", lookup_expr="day"
+    )  # eg. 1, 2, 3, 4, 5, 6, 7
 
     class Meta:
         model = Transaction
         fields = {
-            "title": ["icontains"],
             "amount": ["exact", "lt", "gt"],
             "desc": ["icontains"],
             "_type": ["exact"],
+            "status": ["exact"],
         }
