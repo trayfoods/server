fbf82f0717cd08ded572cec0324d2663dca62504
Divine Ikhuoria <divuzki@gmail.com>
2023-11-23 17:04:02 +0100

    Added OrderFilter

--- product/filters.py
+++ product/filters.py
@@ -1,5 +1,5 @@
 from django_filters import FilterSet, CharFilter
-from product.models import Item
+from product.models import Item, Order
 
 class ItemFilter(FilterSet):
     type = CharFilter(field_name="product_type__urlParamName", lookup_expr="exact")
@@ -16,4 +16,12 @@ class ItemFilter(FilterSet):
             "product_name": ["icontains"],
             "product_slug": ["icontains"],
             "product_price": ["exact", "lt", "gt"], # lt = less than, gt = greater than
+        }
+
+
+class OrderFilter(FilterSet):
+    class Meta:
+        model = Order
+        field = {
+            "order_status":["exact"]
         }
