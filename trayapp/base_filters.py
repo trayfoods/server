0e4939845650fb12c96c06a501790a0e6a42af5c
Divine Ikhuoria <divuzki@gmail.com>
2023-11-27 02:10:48 +0100

    Moved `DateTypeFilter` to trayapp/base_filters.py

---
+++ trayapp/base_filters.py
@@ -0,0 +1,26 @@
+from django.utils import timezone
+from django_filters import CharFilter
+
+DATE_FILTER_TYPES = ["today", "7days", "30days", "Alldays"]
+
+
+class DateTypeFilter(CharFilter):
+    def filter(self, qs, value):
+        # check if the value is not in the list of filter types
+        if value not in DATE_FILTER_TYPES:
+            raise ValueError("Invalid Date Type")
+
+        if value in DATE_FILTER_TYPES:
+            if value == "today":
+                return qs.filter(created_at__date=timezone.now())
+            elif value == "7days":
+                return qs.filter(
+                    created_at__date__gte=timezone.now().date()
+                    - timezone.timedelta(days=7)
+                )
+            elif value == "30days":
+                return qs.filter(
+                    created_at__date__gte=timezone.now().date()
+                    - timezone.timedelta(days=30)
+                )
+        return qs
