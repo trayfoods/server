332f7b05b904640cb5c8fb31fed8e1e166ae5c7a
Divine Ikhuoria <divuzki@gmail.com>
2023-12-01 13:53:45 +0100

    Now Using ImageKit For Media Files

--- trayapp/storage_backends.py
+++ trayapp/storage_backends.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from storages.backends.azure_storage import AzureStorage
+import re
 
 
 class AzureMediaStorage(AzureStorage):
@@ -7,6 +8,11 @@ class AzureMediaStorage(AzureStorage):
     account_key = settings.AZURE_ACCOUNT_KEY
     azure_container = "media"
     expiration_secs = None
+
+    def url(self, name):
+        ret = super(AzureMediaStorage, self).url(name)
+        _ret = re.sub("//[a-z.0-9A-Z]*/", settings.MEDIA_URL, ret)
+        return _ret
 
 
 class AzureStaticStorage(AzureStorage):
