940dc4cbc04f6f1c9198e80326659116429d06a3
Divine Ikhuoria <divuzki@gmail.com>
2024-01-06 19:54:14 +0100

    Update ALLOWED_HOSTS in settings.py

--- trayapp/settings.py
+++ trayapp/settings.py
@@ -92,7 +92,7 @@ if "localhost" in FRONTEND_URL:
 else:
     FRONTEND_URL = "https://%s" % FRONTEND_URL
 
-ALLOWED_HOSTS = ["trayfoods.azurewebsites.net", "api.trayfoods.com", "localhost"]
+ALLOWED_HOSTS = ["api.trayfoods.com", "localhost"]
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = os.environ.get("DATA_UPLOAD_MAX_NUMBER_FIELDS", 2000)
 CSRF_COOKIE_SECURE = DEBUG == False
