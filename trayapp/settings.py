97516b9e65aab36ec3ab2f34691d1ee857aee3b7
Divine Ikhuoria <divuzki@gmail.com>
2024-01-06 13:02:39 +0100

    Add new dependencies and enable long-running refresh tokens in JWT

--- trayapp/settings.py
+++ trayapp/settings.py
@@ -197,6 +197,7 @@ GRAPHQL_JWT = {
     "JWT_HIDE_TOKEN_FIELDS": True,
     "JWT_VERIFY_EXPIRATION": True,
     "JWT_REUSE_REFRESH_TOKENS": True,
+    "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
     "JWT_EXPIRATION_DELTA": timedelta(days=7),
     "JWT_ALLOW_ANY_CLASSES": [
         "graphql_auth.mutations.Register",
