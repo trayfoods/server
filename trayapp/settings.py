f993fd92aeb46ba23f4f8bb174b9a633b849fbc3
Divine Ikhuoria <divuzki@gmail.com>
2024-01-07 14:03:48 +0100

    Add JWT expiration delta of 7 days

--- trayapp/settings.py
+++ trayapp/settings.py
@@ -198,6 +198,7 @@ GRAPHQL_JWT = {
     "JWT_VERIFY_EXPIRATION": True,
     "JWT_REUSE_REFRESH_TOKENS": True,
     "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
+    "JWT_EXPIRATION_DELTA": timedelta(days=7),
     "JWT_AUTH_HEADER_NAME": "HTTP_X_AUTHORIZATION",
     "JWT_ALLOW_ANY_CLASSES": [
         "graphql_auth.mutations.Register",
