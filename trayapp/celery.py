07885cd340feea9f4fc2a64a98c4bcfa62c02335
Divine Ikhuoria <divuzki@gmail.com>
2024-01-18 21:54:19 +0100

    Add broker_connection_retry_on_startup to app.conf

--- trayapp/celery.py
+++ trayapp/celery.py
@@ -9,7 +9,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trayapp.settings")
 
 app = Celery("trayapp", broker=settings.CELERY_BROKER_URL)
 # add broker_connection_retry_on_startup
-app.conf.broker_connection_retry = True
 app.conf.enable_utc = False
 
 app.conf.update(timezone="Africa/Lagos")
@@ -25,3 +24,5 @@ app.conf.beat_schedule = {
 }
 
 app.autodiscover_tasks()
+
+app.conf.broker_connection_retry_on_startup = True
