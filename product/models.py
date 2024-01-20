601108d797a48250317471d7bde27da3aa26a8c9
Divine Ikhuoria <64232397+Divuzki@users.noreply.github.com>
2024-01-20 07:14:12 +0100

    Update models.py
    Commented out push notification for delivery person for now

--- product/models.py
+++ product/models.py
@@ -474,9 +474,9 @@ class Order(models.Model):
                     f"{FRONTEND_URL}/order/{self.order_track_id}/accept-delivery",
                 )
             )
-            delivery_person.profile.send_push_notification(
-                title="You have a new order to deliver",
-            )
+            # delivery_person.profile.send_push_notification(
+            #    title="You have a new order to deliver",
+            # )
 
     # check if a store is linked in any order, if yes, return the orders
     @classmethod
