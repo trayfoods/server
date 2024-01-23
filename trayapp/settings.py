eee6e23f3241e8e556f420ae32a33375d2c612e5
Divine Ikhuoria <divuzki@gmail.com>
2024-01-23 17:24:15 +0100

    Update CORS settings to allow all origins

--- trayapp/settings.py
+++ trayapp/settings.py
@@ -353,12 +353,14 @@ if DEBUG == False:
     ]
 
 
-CORS_ORIGIN_WHITELIST = (
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "https://www.trayfoods.com",
-)
+# CORS_ORIGIN_WHITELIST = (
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://localhost:8000",
+#     "https://www.trayfoods.com",
+# )
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_METHODS = (
     "GET",
