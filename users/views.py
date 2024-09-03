a1c9ec27c6011eb7052de8f51b395ae8179ab0e1
Divine Ikhuoria <divuzki@gmail.com>
2024-09-03 20:53:00 +0100

    Refactor get_filtered_campus function in users/views.py to handle undefined selected_school_id

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from .models import School
 
 def get_filtered_campus(request):
     selected_school_id = request.GET.get("selected_school", None)
-    if selected_school_id is None:
+    if selected_school_id is None or selected_school_id == "undefined":
         return JsonResponse({}, safe=False)
     # campuses looks like this: ["Campus 1", "Campus 2", "Campus 3"]
     data = School.objects.filter(id=selected_school_id).values_list(
